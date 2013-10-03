27b40e346f6b6fb52f12e2e110126f7010fe6008
henry <jaohaohsuan@gmail.com>
2013-10-03 17:48:50 +0800

    change viewModel, model obs seperate operation creation

--- WpfApplication4/Model/LinearFormulaParams.cs
+++ WpfApplication4/Model/LinearFormulaParams.cs
@@ -2,16 +2,6 @@
 
 namespace WpfApplication4.Model
 {
-    public class UnknownFormulaParams : IFormulaParams
-    {
-        public UnknownFormulaParams()
-        {
-            Type = "unknown";
-        }
-
-        public string Type { get; private set; }
-    }
-
     public class LinearFormulaParams : ReactiveObject, IFormulaParams
     {
         private double _BaseIndicator;
---
+++ WpfApplication4/Model/UnknownFormulaParams.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApplication4.Model
+{
+    public class UnknownFormulaParams : IFormulaParams
+    {
+        public UnknownFormulaParams()
+        {
+            Type = "unknown";
+        }
+
+        public string Type { get; private set; }
+    }
+}
